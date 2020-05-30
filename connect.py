d1a351976fe866f17dd83202b535c40e6eb6f311
rschepens <61314016+rschepens@users.noreply.github.com>
2020-05-30 13:52:55 +0200

    Update connect.py

--- connect.py
+++ connect.py
@@ -1,2 +1,25 @@
-print("hello ruud")
-#linux test
+from zeep import Client
+
+#Link naar WSDL schema
+client = Client('http://ec.europa.eu/taxation_customs/vies/checkVatService.wsdl')
+
+
+#Functie voor VIESCHECK
+def wsdl_function(btwNo):
+    country = btwNo[:2]
+    number= btwNo[2:]
+    result = client.service.checkVat(country, number)
+    print(result['name'])
+
+
+
+#wsdl_function(btwCode)
+
+#Een lijst wordt gecreeerd
+listofcode  = list()
+listofcode.append('NL148250749B01')
+listofcode.append('NL823764898B01')
+
+#Door de lijst wordt geloopt en de functie wordt gecalld
+for item in listofcode:
+    wsdl_function(item)
