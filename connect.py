97d91c4ad38f64965a9bcfb6e2bf59e7aabe7437
ruud <ruud.schepens@gmail.com>
2020-05-30 14:56:09 +0200

    3005202

--- connect.py
+++ connect.py
@@ -1,4 +1,5 @@
 from zeep import Client
+import csv
 
 #Link naar WSDL schema
 client = Client('http://ec.europa.eu/taxation_customs/vies/checkVatService.wsdl')
@@ -15,8 +16,13 @@ def wsdl_function(country,btwNo):
 
 #Een lijst wordt gecreeerd
 listofcode  = list()
-listofcode.append(['NL','808669291B01'])
-listofcode.append(['NL','823764898B01'])
+with open('Importlines.csv', 'r', encoding="ISO-8859-1" ) as f: #delimeter zou er aan toegevoegd kunnen worden
+    reader = csv.reader(f)
+    for row in reader:
+        listofcode.append([row[0],row[1]])
+
+#listofcode.append(['NL','808669291B01'])
+#listofcode.append(['NL','823764898B01'])
 
 #Door de lijst wordt geloopt en de functie wordt gecalld
 for item in listofcode:
